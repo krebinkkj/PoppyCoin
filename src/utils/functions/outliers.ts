36d67ec916cb883ee4d3cf9a2239733a629b4145
krebinski j <krebindopagode@gmail.com>
2025-02-13 20:12:16 +0000

    feat: inútil porém útil

---
+++ src/utils/functions/outliers.ts
@@ -0,0 +1,22 @@
+export function filterOutliers(array: number[]) {
+    if (array.length < 8) return array
+
+    let q1: number
+    let q3: number
+
+    const values = array.slice().sort((a, b) => a - b)
+
+    if ((values.length / 4) % 1 === 0) {
+        q1 = (1 / 2) * (values[values.length / 4])
+        q3 = (1 / 2) * (values[values.length * (3 / 4)] + values[values.length * (3 / 4) + 1])
+    } else {
+        q1 = values[Math.floor(values.length / 4 + 1)]
+        q3 = values[Math.ceil(values.length * (3 / 4))]
+    }
+
+    const iqr = q3 - q1
+    const maxValue = q3 + iqr * 1.5
+    const minValue = q1 - iqr * 1.5
+
+    return values.filter((x) => x >= minValue && x <= maxValue)
+}
