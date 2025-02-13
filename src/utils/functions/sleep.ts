dc17b1025881eecdeed2593b4e6075c8d7e4ce90
krebinski j <krebindopagode@gmail.com>
2025-02-13 20:13:36 +0000

    add: função de dormir????

---
+++ src/utils/functions/sleep.ts
@@ -0,0 +1,8 @@
+export default async function sleep(ms: number) {
+    if (ms <= 0) return
+    return new Promise((resolve)=> {
+        setTimeout(() => {
+            resolve(0)
+        }, ms)
+    })
+}
